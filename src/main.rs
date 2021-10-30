ed4a79afa5171eb466e916a5eeaca3d6c581ecc0
Orest Dubay <orest3.dubay@gmail.com>
2021-10-30 16:41:16 +0200

    offset and zoom

--- src/main.rs
+++ src/main.rs
@@ -27,18 +27,30 @@ fn window_conf() -> Conf {
 #[macroquad::main(window_conf)]
 async fn main() -> Result<()> {
     let mut pipeline = Pipeline::new();
+    let mut mouse_origin = None;
     pipeline.load("data.csv").await?;
 //    println!("{}", pipeline.point_data.to_csv_simple());
+    let margin = 10.0f32;
+    let size_x = pipeline.parameters.mesh_width as f32;
+    let size_y = pipeline.parameters.mesh_height as f32;
     loop {
         clear_background(DARKBLUE);
         egui_macroquad::ui(|egui_ctx| {
             egui::Window::new("View setup")
-                .default_pos((820.0, 10.0))
+                .default_pos((2.0*margin+size_x, margin))
                 .show(egui_ctx, |ui| {
                     //ui.label("Test");
                     if ui.button("Zoom all").clicked() {
                         pipeline.zoom_all();
                     }
+                    let mut zoom = pipeline.get_zoom();
+                    ui.add(egui::Slider::new(&mut zoom, 0.5..=10.0));
+                    pipeline.set_zoom(zoom);
+
+                    let mut aspect_ratio = pipeline.get_aspect_ratio();
+                    ui.add(egui::Slider::new(&mut aspect_ratio, 0.5..=2.0));
+                    pipeline.set_aspect_ratio(aspect_ratio);
+
                     egui::Grid::new("Coordinates grid").show(ui, |ui|{
 
                         egui::ComboBox::from_label("X")
@@ -135,7 +147,7 @@ async fn main() -> Result<()> {
                     pipeline.set_density_multiplier(density_multiplier);
 //                    dbg!(&ui.input().pointer.hover_pos());    
                 });
-            egui::Window::new("Data").default_pos((850.0, 370.0))
+            egui::Window::new("Data").default_pos((2.0*margin+size_x, 370.0))
             .show(egui_ctx, |ui| {
                 ui.label(format!("{:?}",ui.input().pointer.hover_pos()));
                 /*
@@ -143,6 +155,34 @@ async fn main() -> Result<()> {
                     ui.label(format!("Index: {:?}",pipeline.mesh.get_index_wide((pos.x-10.0) as usize, (pos.y-10.0) as usize)));
                 }
                 */
+                if let (Some(origin), Some(hover)) = (ui.input().pointer.press_origin(), ui.input().pointer.hover_pos()){
+                    ui.label(format!("Origin: {:?} Hover:{:?}",origin,hover));
+                }
+                if let Some(origin) = ui.input().pointer.press_origin(){
+                    mouse_origin = Some(origin);
+                }
+                
+                if ui.input().pointer.any_released(){
+                    /*
+                    println!("Released {:?}", ui.input().pointer.interact_pos());
+                    println!("Origin: {:?}",mouse_origin);
+                    println!("Hover: {:?}",ui.input().pointer.hover_pos());
+                    */
+                    if let (Some(origin), Some(release)) = (mouse_origin, ui.input().pointer.interact_pos()){
+                        //println!("Offset: {:?} {:?}", origin, release);
+                        let x1 = (origin.x-margin)/size_x;
+                        let y1 = (origin.y-margin)/size_y;
+                        let x2 = (release.x-margin)/size_x;
+                        let y2 = (release.y-margin)/size_y;
+                        if x1>=0.0 && x1<=1.0 && y1>=0.0 && y1<=1.0{
+                            let dx = (x2-x1) as f64;
+                            let dy = (y2-y1) as f64;
+                            //println!("Shift {} {}",dx,dy);
+                            pipeline.relative_offset(dx, dy);
+                        }
+
+                    }
+                }
                 if let Some(pos) = ui.input().pointer.hover_pos(){
                     if let Some(index) = pipeline.mesh.get_index_wide((pos.x-10.0) as usize, (pos.y-10.0) as usize){
                         ui.label(format!("Index: {}",index));
